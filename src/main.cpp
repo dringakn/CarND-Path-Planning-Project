f4da14628a58b0a89191ad29f861b83258140359
AKN <dringakn@gmil.com>
2020-05-02 11:20:49 +0100

    feat: Create trajectory
    define a path made up of (x,y) points that the car will visit sequentially every .02 seconds

--- src/main.cpp
+++ src/main.cpp
@@ -94,17 +94,15 @@ int main() {
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+          // define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
 
-          double dist_inc = 0.5;
+          const double dist_inc = 0.5;
           for (int i = 0; i < 50; ++i) {
-            next_x_vals.push_back(car_x +
-                                  (dist_inc * i) * cos(deg2rad(car_yaw)));
-            next_y_vals.push_back(car_y +
-                                  (dist_inc * i) * sin(deg2rad(car_yaw)));
+            const double next_d = 6;
+            double next_s       = car_s + (i + 1) * dist_inc;
+            vector<double> xy   = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            next_x_vals.push_back(xy[0]);
+            next_y_vals.push_back(xy[1]);
           }
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
